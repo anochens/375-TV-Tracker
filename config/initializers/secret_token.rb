2107c85e9d1cc4143620278cd49ae798165ed279
profh <profh@cmu.edu>
2011-02-16 15:43:11 -0500

    Base Rails project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TvTracker::Application.config.secret_token = '073f6879d51ee92e0fa56b0a72819ce73939124e0f916e531640bf3ff72f54e64a66464d26585a6ca4e002f513310103677cc2bad0f90989e90199cdb5f29902'
