de881c7291617b5af53087f5d7b857e129715313
Andrew Furman <furmanx@gmail.com>
2011-04-17 21:33:44 -0400

    improved formating of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   def create
     @episode = Episode.find(params[:episode_id])
     @comment = @episode.comments.create(params[:comment])
+	@comment.name = current_user.username
+	@comment.save!
     redirect_to episode_path(@episode)
   end
 end
