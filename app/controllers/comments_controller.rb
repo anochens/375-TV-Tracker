f2dd144f0de26463e6b6b486f11203e9def28e36
Alan <anochens@andrew.cmu.edu>
2011-04-20 16:23:12 -0400

    put in case insenstive sorting in series_items

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,8 @@ class CommentsController < ApplicationController
   def create
     @episode = Episode.find(params[:episode_id])
     @comment = @episode.comments.create(params[:comment])
-	@comment.name = current_user.username
-	@comment.save!
+    @comment.name = current_user.username
+	 flash[:error] = "Comment cannot be blank" unless @comment.save
     redirect_to episode_path(@episode)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :episode
+
+  validates_presence_of :body
 end
